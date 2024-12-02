777f1d35809034d0f7ce8bf050bd1fe88a4b2dbd
Krlos <dark7freya@gmail.com>
2024-12-02 09:28:18 -0300

    login mejorado

--- src/app/v2/modules/login/login.component.ts
+++ src/app/v2/modules/login/login.component.ts
@@ -1,18 +1,15 @@
-// src/app/core/components/login/login.component.ts
+// login.component.ts
 
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  ChangeDetectorRef,
-} from '@angular/core';
+import { Component, ChangeDetectorRef, inject } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   Validators,
   ReactiveFormsModule,
+  ValidatorFn,
+  AbstractControl,
 } from '@angular/forms';
-import { LoginService } from '@core/services/login.service';
+import { LoginService } from '@v2/services';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -26,7 +23,7 @@ import { RouterLink } from '@angular/router';
 export class LoginComponent {
   private loginService = inject(LoginService);
   protected fb = inject(FormBuilder);
-  private cdRef = inject(ChangeDetectorRef); // Inyectar ChangeDetectorRef
+  private cdRef = inject(ChangeDetectorRef);
 
   loginForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -36,6 +33,28 @@ export class LoginComponent {
   isLoading = false;
   errorMessage = '';
 
+  // Validation variables
+  hasLength: boolean = false;
+  hasUppercase: boolean = false;
+  hasSpecialChar: boolean = false;
+  hasNumber: boolean = false;
+
+  constructor() {
+    this.loginForm
+      .get('password')
+      ?.valueChanges.subscribe((password: string) => {
+        this.validatePassword(password);
+      });
+  }
+
+  validatePassword(password: string) {
+    this.hasLength = password.length >= 8;
+    this.hasUppercase = /[A-Z]/.test(password);
+    this.hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    this.hasNumber = /[0-9]/.test(password);
+    this.cdRef.markForCheck(); // Ensure the view updates
+  }
+
   onSubmit() {
     if (this.loginForm.invalid) {
       return;
@@ -43,23 +62,21 @@ export class LoginComponent {
 
     this.isLoading = true;
     this.errorMessage = '';
-    this.cdRef.markForCheck(); // Forzar la detección de cambios para actualizar la vista
+    this.cdRef.markForCheck();
 
     const { email, password } = this.loginForm.value;
 
     this.loginService.login({ correo: email, password }).subscribe({
       next: () => {
-        // Manejar el inicio de sesión exitoso
+        // Handle successful login
         this.isLoading = false;
-        // Por ejemplo, redirigir al usuario a otra página
-        // this.router.navigate(['/dashboard']);
-        this.cdRef.markForCheck(); // Forzar la detección de cambios
+        this.cdRef.markForCheck();
       },
       error: (response) => {
         this.errorMessage =
           response?.error?.error || 'Error al iniciar sesión.';
         this.isLoading = false;
-        this.cdRef.markForCheck(); // Forzar la detección de cambios
+        this.cdRef.markForCheck();
       },
     });
   }
