7d33cd1ee0a6dcd3b770e3b743af2bcb6473f773
Krlos <dark7freya@gmail.com>
2024-12-02 14:36:21 -0300

    correction & clean code in sugeridos

--- src/app/v2/components/sugeridos/sugeridos.component.ts
+++ src/app/v2/components/sugeridos/sugeridos.component.ts
@@ -10,20 +10,18 @@ import {
 import { AuthState, Role } from '@shared/enums';
 import DOMPurify from 'dompurify';
 
-import {
-  CourseItemComponent,
-  CourseModalComponent,
-  ArticleItemComponent,
-  ArticleModalComponent,
-  SupplierItemComponent,
-  SupplierModalComponent,
-  FinancingItemComponent,
-  FinancingModalComponent,
-  ProjectsItemComponent,
-  ProjectsModalComponent,
-  StartupItemComponent,
-  StartupModalComponent,
-} from '@v2/components';
+import { CourseItemComponent } from '@v2/components/course-item/course-item.component';
+import { CourseModalComponent } from '../course-modal/course-modal.component';
+import { ArticleItemComponent } from '../article-item/article-item.component';
+import { ArticleModalComponent } from '../article-modal/article-modal.component';
+import { SupplierItemComponent } from '../supplier-item/supplier-item.component';
+import { SupplierModalComponent } from '../supplier-modal/supplier-modal.component';
+import { FinancingItemComponent } from '../financing-item/financing-item.component';
+import { FinancingModalComponent } from '../financing-modal/financing-modal.component';
+import { ProjectsItemComponent } from '../projects-item/projects-item.component';
+import { ProjectsModalComponent } from '../projects-modal/projects-modal.component';
+import { StartupItemComponent } from '../startup-item/startup-item.component';
+import { StartupModalComponent } from '../startup-modal/startup-modal.component';
 
 @Component({
   selector: 'app-sugeridos',
@@ -96,9 +94,11 @@ export class SugeridosComponent implements OnInit {
         let pathMatch = this.paths[this.ruta];
         let searchPath = this.getSearchKey();
 
+        //para articulos por default
+        const pathKeys = Object.keys(this.paths);
+        const randomKey = pathKeys[2];
+
         if (!pathMatch) {
-          const pathKeys = Object.keys(this.paths);
-          const randomKey = pathKeys[2]; //para articulos por default
           pathMatch = this.paths[randomKey];
           searchPath = this.key[randomKey];
         }
@@ -108,14 +108,13 @@ export class SugeridosComponent implements OnInit {
           this.authState() === AuthState.LoggedIn &&
           this.ruta &&
           (this.ruta === 'buscador-cursos' ||
-            this.ruta === 'buscador-proveedores')
+            this.ruta === 'buscador-proveedores') &&
+          (this.modeloMadurez() || this.modeloCaracter())
         ) {
-          if (
-            this.currentUser()?.rol?.nombreRol === Role.Empresa &&
-            this.modeloMadurez()
-          ) {
-            searchPath = this.modeloMadurez()![0].IndustryName ?? searchPath;
-          }
+          searchPath = this.modeloMadurez()![0].IndustryName ?? searchPath;
+        } else {
+          pathMatch = this.paths[randomKey];
+          searchPath = this.key[randomKey];
         }
 
         // Continuamos con la lógica de búsqueda utilizando 'pathMatch'
@@ -143,69 +142,15 @@ export class SugeridosComponent implements OnInit {
     return value;
   }
 
-  getDomain(url: string): string {
-    try {
-      return new URL(url).hostname.replace('www.', '');
-    } catch {
-      return 'URL inválida';
-    }
-  }
-  getPublicationYear(fecha_publicacion: string): number {
-    return this.convertToDate(fecha_publicacion).getFullYear();
-  }
-
-  convertToDate(dateString: string): Date {
-    if (dateString.length !== 8) {
-      console.warn(`Formato de fecha inesperado: ${dateString}`);
-      return new Date(0); // Fecha por defecto
-    }
-    const year = dateString.substring(0, 4);
-    const month = dateString.substring(4, 6);
-    const day = dateString.substring(6, 8);
-    return new Date(`${year}-${month}-${day}`);
-  }
-  sanitizedResumen(resumen: string): string {
-    return DOMPurify.sanitize(resumen);
-  }
-  openLink(link: string): void {
-    try {
-      if (!link) {
-        console.warn('No se proporcionó un link');
-        return;
-      }
-
-      // Verifica si el enlace incluye 'http://' o 'https://'
-      if (!/^https?:\/\//i.test(link)) {
-        link = 'https://' + link; // Puedes usar 'http://' si lo prefieres
-      }
-
-      window.open(link, '_blank');
-    } catch (error) {
-      console.error('Error al abrir el link:', error);
-    }
-  }
   closeModal() {
     this.isModalOpen = false;
     this.dataModal = undefined;
   }
+
   openModal(index: number): void {
     this.currentIndex = index;
     const item = this.sugeridos[this.currentIndex];
     this.isModalOpen = true;
     this.dataModal = item;
-  }
-
-  downloadInfo(link: string): void {
-    try {
-      if (!link) {
-        console.warn('No se proporcionó un link');
-        return;
-      }
-      const urlExcel = 'https://control.nuevoloslagos.org/suppliers/excel/';
-
-      window.open(urlExcel + link, '_blank');
-    } catch (error) {
-      console.error('Error al abrir el link:', error);
-    }
   }
 }
