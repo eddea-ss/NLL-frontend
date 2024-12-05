57ffe009b9ffc4fde0da382cde20f4f81a627468
Krlos <dark7freya@gmail.com>
2024-12-05 11:16:53 -0300

    ceo, href

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { SnackbarComponent } from './v2/components/snackbar/snackbar.component';
+import { Meta, Title } from '@angular/platform-browser';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,32 @@ import { SnackbarComponent } from './v2/components/snackbar/snackbar.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private titleService = inject(Title);
+  private metaService = inject(Meta);
+  private router = inject(Router);
+
   title = 'Nuevo los Lagos';
+
+  constructor() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateCanonicalUrl();
+      });
+  }
+
+  updateCanonicalUrl() {
+    const canonicalUrl = window.location.origin + this.router.url.split('?')[0];
+    let link: HTMLLinkElement | null = document.querySelector(
+      "link[rel='canonical']"
+    );
+
+    if (link === null) {
+      link = document.createElement('link');
+      link.setAttribute('rel', 'canonical');
+      document.head.appendChild(link);
+    }
+
+    link.setAttribute('href', canonicalUrl);
+  }
 }
