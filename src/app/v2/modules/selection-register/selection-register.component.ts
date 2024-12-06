c191986860fec6cc17315e65c1dd18d141de7a99
Krlos <dark7freya@gmail.com>
2024-12-06 17:31:25 -0300

    section register componentt performance

--- src/app/v2/modules/selection-register/selection-register.component.ts
+++ src/app/v2/modules/selection-register/selection-register.component.ts
@@ -13,6 +13,39 @@ export class SelectionRegisterComponent implements OnInit {
   private title = inject(Title);
   private meta = inject(Meta);
 
+  cards = [
+    {
+      title: 'Proveedores',
+      icon: 'dinero',
+      link: '/registro-proveedor',
+      buttonLabel: 'Registrarse',
+      description: `Accede a nuestra plataforma como proveedor y conecta con empresas
+              de la Industria 4.0 para ofrecer tus productos o servicios
+              especializados. Únete a una red de colaboración y aprovecha
+              oportunidades de negocio en crecimiento.`,
+    },
+    {
+      title: 'Empresas de los lagos',
+      icon: 'empresas',
+      link: '/registro-industria',
+      buttonLabel: 'Registrarse',
+      description: `Únete como empresa de la región de Los Lagos para aumentar tu
+              visibilidad y competitividad en la Industria 4.0. Accede a
+              recursos exclusivos, oportunidades de financiamiento y contacto
+              directo con proveedores e innovadores.`,
+    },
+    {
+      title: 'Startups y Emprendedores',
+      icon: 'startups',
+      link: '/registro-persona',
+      buttonLabel: 'Registrarse',
+      description: `Regístrate como Startup o Emprendedor, y explora oportunidades de
+              desarrollo, empleo y formación en el ecosistema de la Industria
+              4.0. Conecta con empresas, accede a cursos en línea y mantente
+              actualizado.`,
+    },
+  ];
+
   ngOnInit(): void {
     this.title.setTitle('Registro | Nuevo Los Lagos');
 
