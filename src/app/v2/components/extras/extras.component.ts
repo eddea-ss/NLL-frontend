782c7892ded392049cd1ce8735a710fbc7bbebb3
Krlos <dark7freya@gmail.com>
2025-01-24 12:32:28 -0300

    fix

--- src/app/v2/components/extras/extras.component.ts
+++ src/app/v2/components/extras/extras.component.ts
@@ -1,14 +1,11 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { ResourceService } from '@v2/services';
-import { TruncatePipe } from '@v2/pipes';
-import { ArticleModalComponent } from '@v2/v2.module';
 
 @Component({
   selector: 'app-extras',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, TruncatePipe, ArticleModalComponent],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './extras.component.html',
   styleUrl: './extras.component.scss',
 })
