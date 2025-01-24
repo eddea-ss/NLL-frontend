2cbc5803ec7646ae841b92470abac2eb21946412
Krlos <dark7freya@gmail.com>
2025-01-24 12:06:04 -0300

    articulos een landing

--- src/app/v2/components/extras/extras.component.ts
+++ src/app/v2/components/extras/extras.component.ts
@@ -1,14 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ResourceService } from '@v2/services';
+import { TruncatePipe } from '@v2/pipes';
+import { ArticleModalComponent } from '@v2/v2.module';
 
 @Component({
   selector: 'app-extras',
   standalone: true,
-  imports: [RouterLink, NavbarComponent],
+  imports: [RouterLink, NavbarComponent, TruncatePipe, ArticleModalComponent],
   templateUrl: './extras.component.html',
   styleUrl: './extras.component.scss',
 })
 export class ExtrasComponent {
   @Input() type: string = 'default';
+
+  @Input() data: any[] = [];
 }
